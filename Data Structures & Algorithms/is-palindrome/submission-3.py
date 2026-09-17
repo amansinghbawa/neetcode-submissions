30d160e4f05d90e4eac9ecdcb384bc40abcad2f5
Aman Preet Singh <amansinghbawa19@gmail.com>
2026-09-17 19:04:00 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ", "").replace("?", "")
+        santized_s = ""
+        for ch in s:
+            if ord(ch) >= ord("a") and ord(ch) <= ord("z") or ord(ch) >= ord("A") and ord(ch) <= ord("Z") or ord(ch) >= ord("0") and ord(ch) <= ord("9"):
+                santized_s+=ch
+        string_len = len(santized_s)
+        for index in range(string_len):
+            if santized_s[index].lower() != santized_s[string_len-index - 1].lower():
+                return False
+        return True
